6d1ebdc131061fd6b8bc50ebb4ba0991b3fb3cc1
JiMo20 <Jmouhtasseb@gmail.com>
2024-07-15 13:55:53 +0400

    modified find function

--- cal2.js
+++ cal2.js
@@ -17,22 +17,52 @@ let clearResult = () => {
   display.value = "";
 };
 
-function add(x, y) {
-  // let add =(x,y) => {}
-  return x + y;
-}
-function multiply(x, y) {
-  //let multiply =(x,y) => {}
-  return x * y;
-}
-function calculate(a, b, operation) {
-  //let calculate = (a,b, operation) => {}
-  return operation(a, b);
-}
-const addition = calculate(5, 3, add);
-console.log(addition);
-const multiplication = calculate(5, 3, multiply);
-console.log(multiplication);
+let getUsers = () => {
+  let users = [
+    { userName: "Ahmed", email: "Ahmed@gmail.com" },
+    { userName: "Ali", email: "Ali@gmail.com" },
+  ];
+  return users;
+};
+let findUser = (userSearchedfor) => {
+  let users = getUsers();
+  let user = users.find((m) => {
+    return m.userName == userSearchedfor;
+  });
+  //   console.log(user);
+  return user.userName;
+};
+
+console.log(findUser("Ali"));
+
+// let greet_one = "Hello";
+// let greet_two = "World";
+// console.log(greet_one);
+
+// setTimeout(function () {
+//   console.log("Asynchronous");
+// }, 10000);
+// console.log(greet_two);
+
+/**
+ * callback function
+ */
+// function add(x, y) {
+//   // let add =(x,y) => {}
+//   return x + y;
+// }
+// function multiply(x, y) {
+//   //let multiply =(x,y) => {}
+//   return x * y;
+// }
+// function calculate(a, b, operation) {
+//   //let calculate = (a,b, operation) => {}
+//   return operation(a, b);
+// }
+// const addition = calculate(5, 3, add);
+// console.log(addition);
+// const multiplication = calculate(5, 3, multiply);
+// console.log(multiplication);
 
 // console.log(eval("3*3"));
 
